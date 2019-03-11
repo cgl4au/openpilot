b215eb7f77e9b774045f668fb024fbcaa85091f6
George Hotz <george@comma.ai>
2019-03-10 23:05:36 -0400

    remove resistance. only 2 params

--- selfdrive/kegman_conf.py
+++ selfdrive/kegman_conf.py
@@ -22,7 +22,6 @@ class kegman_conf():
         self.config.update({"tuneGernby":"0"})
         self.config.update({"react":"-1"})
         self.config.update({"damp":"-1"})
-        self.config.update({"resist":"-1"})
         self.config.update({"Kp":"-1"})
         self.config.update({"Ki":"-1"})    
         self.element_updated = True 
@@ -40,7 +39,7 @@ class kegman_conf():
       self.config = {"cameraOffset":"0.06", "lastTrMode":"1", "battChargeMin":"60", "battChargeMax":"70", \
                      "wheelTouchSeconds":"180", "battPercOff":"25", "carVoltageMinEonShutdown":"11800", \
                      "brakeStoppingTarget":"0.25", "tuneGernby":"0", "react":"-1", "damp":"-1", \
-                     "resist":"-1", "Kp":"-1", "Ki":"-1"}
+                     "Kp":"-1", "Ki":"-1"}
       self.write_config(self.config)
     return self.config
 
